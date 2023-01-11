653c7a285e057f513ea0154bc5fd0dab5e38da82
vijay samrath <vijaysamrath462@gmail.com>
2023-01-11 21:03:35 +0530

    Initial commit

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x3b122cad49C4B8CA7Db193ad3e679aec2B4e3D70";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-nft-marketplace.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
